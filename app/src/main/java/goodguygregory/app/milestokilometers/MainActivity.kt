411342b7585b618f17bd1c89d9dbc28fe1994ac8
GoodGuyGregory <greg.witt625@gmail.com>
2021-09-22 15:42:59 -0400

    adds working logic for mvp

--- app/src/main/java/goodguygregory/app/milestokilometers/MainActivity.kt
+++ app/src/main/java/goodguygregory/app/milestokilometers/MainActivity.kt
@@ -2,7 +2,10 @@ package goodguygregory.app.milestokilometers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,32 +15,52 @@ class MainActivity : AppCompatActivity() {
 
 
         // assuming the user input is Kilometers
-        fun calculateKilometers(distanceProvided :String): Double {
+        fun calculateKilometersFromMiles(distanceProvided: String): Double {
             // convert to double
-            distanceProvided = distanceProvided.toDouble(distanceProvided)
-            val MilesPerKM: Double = 0.62137
-            var ConvertedKM = MilesPerKM * distanceProvided
-            return ConvertedKM
+            var distanceProvided = distanceProvided.toDouble()
+            val MilesPerKM = 0.62137
+            var ConvertedKM = distanceProvided / MilesPerKM
+            val ConvertedKMDec = BigDecimal(ConvertedKM).setScale(2, RoundingMode.HALF_EVEN)
+            return ConvertedKMDec.toDouble()
 
         }
 
         // assumes the user input is Miles
-        fun calculateMiles(distanceProvided :String): Double {
+        fun calculateMilesFromKilometers(distanceProvided: String): Double {
             // convert to double
-            distanceProvided = distanceProvided.toDouble(distanceProvided)
-            val MilesPerKM: Double = 0.62137
-            var ConvertedMiles = distanceProvided / MilesPerKM
-            return ConvertedMiles
+            var distanceProvided = distanceProvided.toDouble()
+            val MilesPerKM = 0.62137
+            var ConvertedMiles = distanceProvided * MilesPerKM
+            val decConversionMiles = BigDecimal(ConvertedMiles).setScale(2, RoundingMode.HALF_EVEN)
+            return decConversionMiles.toDouble()
 
         }
 
+        fun conversionButtonClick() {
+            val distanceProvided: String = distanceField.text.toString()
+            // determine clicked behavior
+            if (milesSwitch.isChecked() && !kilometerSwitch.isChecked()) {
+                var milesFromKm:String =  calculateKilometersFromMiles(distanceProvided).toString()
+                convertedValue.text = " $distanceProvided miles is  $milesFromKm kilometers"
 
+            }
+
+            if (!milesSwitch.isChecked() && kilometerSwitch.isChecked()) {
+                var kmToMiles:String =  calculateMilesFromKilometers(distanceProvided).toString()
+                convertedValue.text = " $distanceProvided Kilometers is $kmToMiles miles"
+            }
+
+            if (!milesSwitch.isChecked() && !kilometerSwitch.isChecked()) {
+                convertedValue.text = "Select Conversion Format."
+            }
+
+            if (milesSwitch.isChecked() && kilometerSwitch.isChecked()) {
+                convertedValue.text = "Select ONE Conversion Format."
+            }
+        }
 
         conversionButton.setOnClickListener {
-            val distanceProvided :String = distanceField.text.toString()
-
-
+            conversionButtonClick()
         }
-
     }
 }
